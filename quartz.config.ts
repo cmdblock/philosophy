7d3209efeb1990120f83b523d04313053c5aa6e3
cmdblock <wow328@163.com>
2026-03-20 00:03:57 +0800

    fix: 修改了 baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "cmdblock.github.io/philosophy",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
